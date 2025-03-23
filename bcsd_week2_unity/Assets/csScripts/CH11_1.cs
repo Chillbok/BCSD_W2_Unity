42d7926735f40f9517137c631f78882741fb69f9
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-03-23 22:25:44 +0900

    실행이 안되던 문제 해결
    `ClassPractice.cs` 파일 삭제.
    `CH11_1.cs` 파일 추가.
    
    클래스에 대한 이해도가 부족해 코드를 실행해도 `Debug.Log`가 실행되지 않는 문제가 있었음. 해결 완료.

---
+++ bcsd_week2_unity/Assets/csScripts/CH11_1.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CH11_1 : MonoBehaviour
+{
+    public Player player01;
+    public Player player02;
+
+    private void Awake()
+    {
+        player01 = new Player();
+        player01.TakeDamage(100);
+        Debug.Log($"{player01.ID}가 데미지 입음(현재 체력 {player01.currentHP})");
+    }
+}
+
+public class Player
+{
+    public string ID = "이준호";
+    public int currentHP = 1000;
+
+    public void TakeDamage(int damage)
+    {
+        if(currentHP > damage)
+        {
+            currentHP -= damage;
+        }
+    }
+
+    public string SetID(string ID)
+    {
+        Debug.Log($"ID: {ID}");
+        Debug.Log($"ID: {this.ID}");
+        return ID;
+    }
+}
+
+public class Enemy
+{
+    private Player player;
+    public void AttackToTarget(Player target)
+    {
+        target.TakeDamage(100);
+    }
+}
